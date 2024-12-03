7e9b683e829d9701d4ab90b39f4b2e37ca4ff3b1
Иван <ivan702007@gmail.com>
2024-12-03 13:51:02 +0300

    ДЗ 6

---
+++ module_1_6_1.py
@@ -0,0 +1,5 @@
+quantity_homework = 12
+quantity_hours = 1.5
+course_name = "Python"
+time_for_one_task = quantity_hours / quantity_homework
+print("Курс:", course_name,", всего задач:", quantity_homework, ", затрачено часов:", quantity_hours, ", среднее время выполнения:", time_for_one_task, "часов.")
