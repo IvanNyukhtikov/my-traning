db67e18843b1838ed60b4247ab4eb014ea5995df
Иван <ivan702007@gmail.com>
2024-12-03 14:22:08 +0300

    ДЗ 7

--- module_1_6.py
+++ module_1_6.py
@@ -1,5 +1,12 @@
-quantity_homework = 12
-quantity_hours = 1.5
-course_name = "Python"
-time_for_one_task = quantity_hours / quantity_homework
-print("Курс:", course_name,", всего задач:", quantity_homework, ", затрачено часов:", quantity_hours, ", среднее время выполнения:", time_for_one_task, "часов.")
+my_dict = {'Вася': 1990, 'Коля': 1995, 'Настя': 1994}
+print(my_dict, my_dict['Вася'], my_dict.get('Оля'), sep='\n')
+my_dict.update({'Маша': 2000, 'Юля': 2001})
+print(my_dict.pop('Вася'))
+print(my_dict)
+
+my_set = {1, 'String', 3, 4, 5.2, 1, 1, 'String',3, 5.2}
+print(my_set)
+list_ = [5,6]
+my_set.update(list_)
+my_set.discard(1)
+print(my_set)
